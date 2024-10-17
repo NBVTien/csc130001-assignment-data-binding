1e2bfe59f80837e6ddafac383d784fc5bd9184e8
NBVTien <vuongtiennguyenbui@gmail.com>
2024-10-17 08:40:25 +0700

    init

---
+++ Helpers/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week04.Helpers;
+
+public class Book : INotifyPropertyChanged 
+{
+    public string Name { get; set; }
+    public string CoverImageSource { get; set; }
+    public string Author { get; set; }
+    public int PublishedYear { get; set; }
+    public string ISBN { get; set; }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+}
